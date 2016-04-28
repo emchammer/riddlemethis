300d39628cb4ed4af972985f17729449df520006
emchammer <frabbit1@hotmail.com>
2016-04-28 12:55:35 +0100

    yaaaayy finally

--- riddle/urls.py
+++ riddle/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     url(r'^infinity$', views.riddle_four, name='riddle_four'),
     url(r'111312211f$', views.riddle_five, name='riddle_five'),
     url(r'decaf', views.riddle_six, name='riddle_six'),
-    url(r'yes', views.riddle_six, name='riddle_seven'),
+    url(r'yes', views.riddle_seven, name='riddle_seven'),
 ]
